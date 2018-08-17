0f23426fa7e14c77cfa167878e7c8a663059708b
barunp <barunp@gmail.com>
2018-08-16 22:05:21 -0400

    Add project files.

---
+++ Interface/INterface.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IService
+    {
+        string ServiceMethod();
+    }
+
+    public class ClaimService : IService
+    {
+        public string ServiceMethod()
+        {
+            return " ClaimService is running";
+        }
+    }
+
+    public class AdjudificationService : IService
+    {
+        public string ServiceMethod()
+        {
+            return " AdjudificationService is running";
+        }
+    }
+
+    public class PaymentService : IService
+    {
+        public string ServiceMethod()
+        {
+            return " PaymentService is running";
+        }
+    }
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,52 @@
+﻿using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            BusinessLogicImplementation businessLogicImplementation = new BusinessLogicImplementation(new ClaimService());
+            businessLogicImplementation = new BusinessLogicImplementation(new AdjudificationService());
+            businessLogicImplementation = new BusinessLogicImplementation(new PaymentService());
+            
+
+            UnityContainer unityContainer = new UnityContainer();
+            unityContainer.RegisterType<IService, ClaimService>();
+            unityContainer.RegisterType<IService, AdjudificationService>();
+            unityContainer.RegisterType<IService, PaymentService>();
+
+
+            AdjudificationService obj = unityContainer.Resolve<AdjudificationService>();
+            obj.ServiceMethod();
+            IService obj1 = unityContainer.Resolve<ClaimService>();
+            obj1.ServiceMethod();
+            IService obj2 = unityContainer.Resolve<PaymentService>();
+            obj2.ServiceMethod();
+           
+            Console.ReadLine();
+
+
+
+        }
+    }
+
+
+    public class BusinessLogicImplementation
+    {
+        private IService client;
+        public BusinessLogicImplementation(IService client)
+        {
+            this.client = client;
+            Console.WriteLine("Constructor Injection Injection ==> Current Service: {0}", client.ServiceMethod());
+        }
+    }
+}
